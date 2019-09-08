35abde0018bca1e5376dca701b676c0217aeeaeb
leelok1998 <1078377877@qq.com>
2019-09-08 13:42:39 +0800

    项目配置完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    //强制使用单引号
+    quotes: ['error', 'single'],
+    //强制不使用分号结尾
+    semi: ['error', 'never']
   },
   parserOptions: {
-    parser: "babel-eslint"
-  },
-  overrides: [
-    {
-      files: ["**/__tests__/*.{j,t}s?(x)"],
-      env: {
-        mocha: true
-      }
-    }
-  ]
-};
+    parser: 'babel-eslint'
+  }
+}
